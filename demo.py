a4f64fa0597d258d03f29d2e385efe7612d2f5e2
Monkeydtan <gamger12@gmail.com>
2024-09-23 15:20:59 +0700

    modify more

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ from tkinter.ttk import Combobox
 
 app = tk.Tk()
 app.title("Multitask")
-app.geometry("500x500")
+app.geometry("500x700")
 
 # textbox
 header = tk.Label(app,text="What happened ?")
@@ -21,8 +21,9 @@ sub_head.pack()
 header2 = tk.Label(app,text="Select your state")
 header2.pack(pady=(10,0))
 
+items = tk.StringVar()
 tools = ['C','C++','C#','Python','Dart','Javascript']
-combo_box = Combobox(app,values=tools)
+combo_box = Combobox(app,values=tools,textvariable=items)
 combo_box.pack()
 combo_box.current(0) # ตั้งค่าเริ่มต้นเป็นตัวเลือกแรก
 
@@ -36,5 +37,18 @@ for i in libraries:
     list_box.insert(tk.END,i)
 list_box.pack()
 
+def selected():
+    data1 = text_box.get("1.0",tk.END).strip() #เก็บข้อความตั้งแต่อักขระแรกจนถึงอันสุดท้ายและเก็บไว้ที่ตัวแปร data
+    data2 = items.get()
+    data_list = list_box.curselection() # .curselection() คือ การเลือก
+    data3 = list_box.get(data_list)
+    empty_text.config(text=f"Name: {data1}\nYour language: {data2}\nLibrary: {data3}") #แสดงข้อความในempty_text
+    
+btn = tk.Button(app,text="Select",command=selected)
+btn.pack(pady=(10,0))
+
+# พื้นที่การแสดงข้อความที่พิมพ์ลงไปใน text box
+empty_text = tk.Label(app)
+empty_text.pack(pady=(10,0))
 
 app.mainloop()
